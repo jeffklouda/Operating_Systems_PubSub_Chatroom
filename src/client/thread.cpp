7e6a2386ec8c01297d320820ec05b6f51eebbaa9
Matthew Anthony D'Alonzo <mdalonz1@nd.edu>
2017-10-05 22:05:42 -0400

    thing

---
+++ src/client/thread.cpp
@@ -0,0 +1,29 @@
+// thread.cpp
+// Implementation of thread class
+// Authors: Matthew D'Alonzo and Jeff Klouda
+
+#include "client.h"
+#include <pthread.h>
+
+Thread::Thread(){
+
+}
+
+Thread::~Thread(){
+
+}
+
+void Thread::start(thread_func func, void* arg){
+	//switch (func){
+		//case (publish): 
+		//pthread_create(&thread_var, NULL, this->publish
+	//}
+}
+
+void Thread::join(void** result){
+
+}
+
+void Thread::detach(){
+
+}
