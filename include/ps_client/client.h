b45fd024db9dde14e0aea21fcbd7b232a14c029f
Matthew Anthony D'Alonzo <mdalonz1@nd.edu>
2017-10-05 21:16:44 -0400

    Added prototypes to header file

--- include/ps_client/client.h
+++ include/ps_client/client.h
@@ -1,5 +1,54 @@
 // client.h: PS Client Library -------------------------------------------------
 
+//NECESSARY INCLUDES
+#include <string.h>
+#include <string>
+
 #pragma once
+
+enum func { publish, retrieve, callback };
+
+struct Message{
+    std::string type;
+    std::string topic;
+    std::string sender;
+    size_t      nonce;
+    std::string body;
+};
+
+class Client{
+    public:
+        Client(const char*, const char*, const char*);
+        ~Client();
+        void publish(const char*, const char*, size_t);
+        void subscribe(const char*, Callback*);
+        void unsubscribe(const char*);
+        void disconnect();
+        void run();
+        bool shutdown();
+    private:
+        const char* host;
+        const char* port;
+        const char* cid;
+};
+
+class Callback{
+    public:
+        Callback();
+        ~Callback();
+        virtual void run(Message&);
+};
+
+class Thread{
+    public:
+        Thread();
+        ~Thread();
+        void start(thread_func, void*);
+        void join(void**);
+        void detach();
+    private:
+        thread_func func;
+        void*       arg;
+};
 
 // vim: set expandtab sts=4 sw=4 ts=8 ft=cpp: ----------------------------------
