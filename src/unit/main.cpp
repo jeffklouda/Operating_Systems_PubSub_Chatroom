57c3b5970c167e93840468191970028577ac47be
Matthew Anthony D'Alonzo <mdalonz1@nd.edu>
2017-10-24 00:01:52 -0400

    this

--- src/unit/main.cpp
+++ src/unit/main.cpp
@@ -3,6 +3,10 @@
 #include <gtest/gtest.h>
 
 // C++ PROJECT INCLUDES
+#include <ps_client/client.h>
+
+TEST(subscribe, subscribe_added_to_queue){
+}
 
 int main(int argc, char* argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
